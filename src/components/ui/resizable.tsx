a12bfd67d8fcedb62285b2e6d2f51c03c0344a51
Sidharth Gaurav <sidhartraj221@gmail.com>
2026-01-07 07:21:36 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Grip, PanelLeft, PanelRight } from "lucide-react"
+import { Grip } from "lucide-react"
 import * as React from "react"
 import { ImperativePanelHandle, Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
 

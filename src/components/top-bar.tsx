4335d88b5bf97990a3b81e93c585b91b585a04e6
Sidharth Gaurav <sidhartraj221@gmail.com>
2026-01-07 06:10:07 +0000

    add togke in the side bar to open and cloded

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -6,7 +6,7 @@ import { UserNav } from './user-nav';
 export function TopBar() {
   return (
     <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-      <SidebarTrigger className="md:hidden" />
+      <SidebarTrigger />
       <div className="relative ml-auto flex-1 md:grow-0">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
